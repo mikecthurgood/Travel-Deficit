7adbde8f139d48e906e98dcd93786e514246606c
Mike Thurgood <mikecthurgood@gmail.com>
2019-11-21 10:24:32 +0000

    user login functionality

--- src/App.js
+++ src/App.js
@@ -3,21 +3,57 @@ import './App.css';
 import HomePage from './Components/HomePage'
 import { Route, withRouter } from 'react-router-dom'
 import NavBar from './Components/NavBar'
+import Login from './Components/Login'
+import API from './Helpers/API';
 
-const App = () => {
-  return (
-    <div className='site'>
-      <NavBar />
-      <div className='site-body'>
-        <Route path="/" render={(routerProps) =>
-          <HomePage
-            {...routerProps}
-          />
-        } />
+class App extends React.Component {
+
+  state = {
+    username: "",
+    user_image_url: "",
+  }
+
+  guestLogin = () => {
+    this.setState({ username: 'Guest' })
+    this.props.history.push('/')
+  }
+
+  authenticate = (resp) => {
+    console.log(resp);
+    if (resp.error) throw Error(resp.error)
+    API.login(resp.id, { name: resp.name, image_url: resp.picture.data.url, fb_id: resp.id })
+    this.setState({ username: resp.name, user_image_url: resp.picture.data.url })
+    this.props.history.push('/')
+  };
+
+  render() {
+    return (
+      <div className='site'>
+        <NavBar
+          username={this.state.username}
+          userImage={this.state.user_image_url}
+        />
+        <div className='site-body'>
+          <Route exact path="/" render={(routerProps) =>
+            <HomePage
+              {...routerProps}
+              user={this.state.username}
+            />
+          } />
+          <Route path="/login" render={(routerProps) =>
+            <Login
+              {...routerProps}
+              guestLogin={this.guestLogin}
+              user={this.state.username}
+              authenticate={this.authenticate}
+            />
+          } />
+        </div>
       </div>
-    </div>
 
-  );
+    );
+  }
+
 }
 
 export default withRouter(App);
